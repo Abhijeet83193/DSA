5474b920799f122315db9c7b9358c5ca85535a3f
Abhijeet Dhokne <abhijeetdhokne1474@gmail.com>
2025-06-26 20:33:54 +0530

    array input / output

---
+++ arrays.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class arrays {
+    //input
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int marks[] = new int[100];
+        System.out.print("phy: ");
+        marks[0] = sc.nextInt();
+        System.out.print("che: ");
+        marks[1] = sc.nextInt();
+        System.out.print("math: ");
+        marks[2] = sc.nextInt();
+        int avg;
+        avg = (marks[0]+marks[1]+marks[2])/3;
+    //output
+        System.out.println("average marks is : "+avg);
+        sc.close();
+
+    }
+}
