5a8b890fbb28a18eb8948bd98a9d00553d4e16bf
Abhijeet Dhokne <abhijeetdhokne1474@gmail.com>
2025-07-02 10:14:20 +0530

    subarrays(sum, max&min)

--- subarrays.java
+++ subarrays.java
@@ -4,12 +4,33 @@
 
 public class subarrays {
     public static void subarr(int numbers[]) {
-        
+        int maxsum = Integer.MIN_VALUE;
+        int minsum = Integer.MAX_VALUE;
+        for(int start=0; start<numbers.length; start++){
+        int sum = 0;
+            for(int end=start; end<numbers.length; end++){
+                sum += numbers[end];
+                for(int k=start; k<=end; k++){
+                    System.out.print(numbers[k]+" ");
+                }
+                System.out.println(" => sum is: "+sum);
+            }
+            System.out.println();
+            
+            if (sum>maxsum) {
+                maxsum = sum;
+            }
+
+            if (sum<minsum) {
+                minsum = sum;
+            }
+
+        }  
+        System.out.println("maxsum is: "+maxsum);
+        System.out.println("minsum is: "+minsum);
     }
     public static void main(String[] args) {
         int numbers[] = {2,4,6,8,10};
         subarr(numbers);
-
-        
     }
 }
